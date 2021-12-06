7619a32b3557360ce53de6dabd0a4f4c0b0eed6d
Tyler Porter <tyler.b.porter@gmail.com>
2021-12-06 10:26:03 -0500

    Include revised code for challenge 6

--- 2021/06/main.rb
+++ 2021/06/main.rb
@@ -1,35 +1,24 @@
 require_relative "../../utils.rb"
 
-def part_1_solution(strs, amt: 80, print: true)
+def part_1_solution(strs, iterations: 80, print: true)
   fish_list = strs.first.split(",").map(&:to_i)
-  fish_counts = {}
 
-  (-1..8).each do |value|
-    fish_counts[value] = fish_list.count(value) || 0
+  fish_counts = (0..8).map { |value| fish_list.count(value) || 0 }
+
+  iterations.times do |day|
+    expired_fish = fish_counts.shift
+
+    fish_counts << expired_fish
+    fish_counts[6] += expired_fish
   end
 
-  amt.times do |day|
-    new_fish_counts = (-1..8).map { |value| [value, 0] }.to_h
+  puts "PART 1: #{fish_counts.sum}" if print
 
-    fish_counts.each do |key, value|
-      new_fish_counts[key - 1] = value
-    end
-
-    new_fish_counts[6] += new_fish_counts[-1]
-    new_fish_counts[8] += new_fish_counts[-1]
-    new_fish_counts[-1] = 0
-    new_fish_counts.delete(-2)
-
-    fish_counts = new_fish_counts
-  end
-
-  puts "PART 1: #{fish_counts.values.sum}" if print
-
-  fish_counts.values.sum
+  fish_counts.sum
 end
 
 def part_2_solution(strs)
-  puts "PART 2: #{part_1_solution(strs, amt: 256, print: false)}"
+  puts "PART 2: #{part_1_solution(strs, iterations: 256, print: false)}"
 end
 
 test_strs = [
