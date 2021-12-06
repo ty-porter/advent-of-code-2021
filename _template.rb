b3345c9a1ceb45c03c842ff367c0f62ff41a6acc
Tyler Porter <tyler.b.porter@gmail.com>
2021-12-06 00:47:37 -0500

    Update my template

--- _template.rb
+++ _template.rb
@@ -1,9 +1,16 @@
 require_relative "../../utils.rb"
 
-def part_1_solution
+def part_1_solution(values)
+  value = 0
 
+  puts "PART 1: #{value}"
 end
 
-def part_2_solution
+def part_2_solution(values)
+  value = 0
 
+  puts "PART 2: #{value}"
 end
+
+part_1_solution []
+part_2_solution []
